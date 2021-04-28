0167b7a9bbba9b6f3bf43c90ad119b46e3206180
sujiabc@123 <p.p.sujithkumarr@gmail.com>
2021-04-28 09:37:30 +0530

    added angular code

--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -4,23 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FormsComponent } from './forms/forms.component';
-import { HomeComponent } from './home/home.component';
-import { ProjectComponent } from './project/project.component';
 import { ServicesComponent } from './services/services.component';
+import { FormsComponent } from './forms/forms.component';
+import { ProjectComponent } from './project/project.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ServicesComponent,
     FormsComponent,
-    HomeComponent,
     ProjectComponent,
-    ServicesComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-angular-app/src/app/home/home.component.ts
+++ my-angular-app/src/app/home/home.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  title = "Templates and Directives";
+  heading = "Courses Offered";
+  courses = ["HTML","CSS","AJAX","JSON","Angular"];
+  imgUrl = '../../assets/types-of-data-binding.png';
+  users = [ 
+            {"name":"sujith","email":"sujith@gmail.com","isworking":false},
+            {"name":"Mahesh","email":"mahesh@gmail.com","isworking":true},
+            {"name":"suresh","email":"suresh@gmail.com","isworking":false}
+  ];
+  userprofile = {
+    name:"sujith",
+    email:"sujith@gmail.com",
+    salary : 35000,
+    dob : new Date(1999,4,10)
+
+  }
+  colSpan = 3;
+  isActive:boolean = true;
+  message = "Reading Books"
+  todos : any [] = [];
+  todo : string = "";
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+  changeStatus(){
+      this.isActive = !this.isActive;
+  }
+  displaytodo(){
+    if(this.todo != ""){
+      this.todos.push(this.todo);
+      this.todo = "";
+    }
+  }
+  removeTodo(index : number){
+    this.todos.splice(index,1);
 
+  }
+   
 }
