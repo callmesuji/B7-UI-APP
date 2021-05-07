b12642d8407108d1ce15cbfaca5f986ea09c8049
sujiabc@123 <p.p.sujithkumarr@gmail.com>
2021-05-07 10:09:25 +0530

    added angular in ui_app

--- my-angular-app/src/app/model/ipl-module/player.model.ts
+++ my-angular-app/src/app/model/ipl-module/player.model.ts
@@ -4,3 +4,17 @@ export interface Player{
     price : number,
     role : string
 }
+// constructor(private name_ : string,private label_ : string ,private price_:number ,private role_ : string ){
+// }
+// get name(){
+//     return this.name_;
+// }
+// get label(){
+//     return this.label_;
+// }
+// get price(){
+//     return this.price_;
+// }
+// get role(){
+//     return this.role_;
+// }
--- my-angular-app/src/app/project/project.component.ts
+++ my-angular-app/src/app/project/project.component.ts
@@ -1,55 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { GoogleChartInterface } from 'ng2-google-charts';
-import { IplService } from '../ipl/ipl.service';
-import { Player } from '../model/ipl-module/player.model';
+import {Component, OnInit} from '@angular/core';
+import {GoogleChartInterface} from 'ng2-google-charts';
+import {IplService} from '../ipl/ipl.service';
+import {Player} from '../model/ipl-module/player.model';
 
 
-@Component({
-  selector: 'app-project',
-  templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
-})
+@Component({selector: 'app-project', templateUrl: './project.component.html', styleUrls: ['./project.component.css']})
 export class ProjectComponent implements OnInit {
- labels : string[] = [];
- players  : Player[] = [];
- chartData : any[] = []; 
- selectedLabel : string = "";
-  constructor(private iplservice : IplService) { }
-  public pieChart: GoogleChartInterface = {
-    chartType: 'PieChart',
-    dataTable:this.chartData,
-    //firstRowIsData: true,
-    options: {'title': 'Tasks'},
-  };
-
-  ngOnInit(): void {
-    this.iplservice.getTeamLabels().subscribe(res =>
-
-      this.labels = res["labels"]);
- 
-  }
-  onsubmit(){
-    this.iplservice.getPlayersByTeamLabels(this.selectedLabel).subscribe(res =>{
-      this.players = res;
-     // console.log(res);
-    })
-    this.iplservice.getRoleCountByTeamLabel(this.selectedLabel).subscribe(res => {
-      this.chartData = [];
-      this.chartData.push(["Role","Count"])
-      res.forEach(ele=>{
-          this.chartData.push([ele["role"],ele["count"]]);
-      })
-      console.log("Chart data",this.chartData);
-      this.pieChart={
+    labels : string[] = [];
+    players : Player[] = [];
+    chartData : any[] = [];
+    selectedLabel : string = "";
+    constructor(private iplservice : IplService) {}
+    public pieChart : GoogleChartInterface = {
         chartType: 'PieChart',
         dataTable: this.chartData,
-        //firstRowIsData: true,
-        options: {'title': 'Tasks'},
-      };
-  
-    })
+        // firstRowIsData: true,
+        options: {
+            'title': 'Tasks'
+        }
+    };
 
+    ngOnInit(): void {
+        this.iplservice.getTeamLabels().subscribe(res => this.labels = res["labels"]);
 
-  }
+    }
+    onsubmit() {
+        this.iplservice.getPlayersByTeamLabels(this.selectedLabel).subscribe(res => {
+            this.players = res;
+
+        })
+        this.iplservice.getRoleCountByTeamLabel(this.selectedLabel).subscribe(res => {
+            this.chartData = [];
+            this.chartData.push(["Role", "Count"])
+            res.forEach(ele => {
+                this.chartData.push([
+                    ele["role"], ele["count"]
+                ]);
+            })
+
+            this.pieChart = {
+                chartType: 'PieChart',
+                dataTable: this.chartData,
+                // firstRowIsData: true,
+                options: {
+                    'title': 'Tasks'
+                }
+            };
+
+        })
+    }
+    getTotalAmount() {
+        let total = 0;
+        if (this.players.length != 0) {
+            total = this.players.map(p => p.price).reduce((a, b) => a + b);
+        }
+        return total;
+
+    }
+    getAvgAmount() {
+        return this.getTotalAmount() / this.players.length
+    }
 
 }
